98d895ac97eea1c68b88f40504813bc013ab3ee1
Mario Guillen <marioagr95@gmail.com>
2025-09-10 22:06:15 -0600

    Option for commands history to handle the allowance of not having duplicates

--- lua/laravel/providers/history_provider.lua
+++ lua/laravel/providers/history_provider.lua
@@ -16,12 +16,23 @@ end
 
 function provider:boot(app)
   local group = vim.api.nvim_create_augroup("laravel.history", {})
+  local allows_duplicates = app("laravel.services.config")("features").history.allow_duplicates
 
   vim.api.nvim_create_autocmd({ "User" }, {
     group = group,
     pattern = { "LaravelCommandRun" },
     callback = function(ev)
-      app("history"):add(ev.data.job_id, ev.data.cmd, ev.data.args, ev.data.options)
+      if allows_duplicates then
+        app("history"):add(ev.data.job_id, ev.data.cmd, ev.data.args, ev.data.options)
+      else
+        local already_exists = vim.tbl_contains(app("history"):get(), function(v)
+          return vim.deep_equal(v.args, ev.data.args)
+        end, { predicate = true })
+
+        if not already_exists then
+          app("history"):add(ev.data.job_id, ev.data.cmd, ev.data.args, ev.data.options)
+        end
+      end
     end,
   })
 end
